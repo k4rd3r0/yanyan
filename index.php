169fae5544858b9eb46f0a3c635c2b3a9ced6420
k4rd3r0 <54228691+k4rd3r0@users.noreply.github.com>
2022-09-23 23:22:18 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Boo</title>
+  </head>
+  <body>
+    <center>
+      <br><br><br><br><br><br><br><br><br>
+    <b><p>Click <a href="/ily.html">Here</a>.</p>
+  </body>
+</html>
